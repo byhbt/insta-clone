3c1daaf305b03571e4d907c266a8b147bd1e34e4
Thanh Huynh <huynhbathanh@gmail.com>
2021-02-17 10:31:15 +0700

    Add validation for attachment

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,7 @@ class Photo < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  validates :image, attached: true, content_type: %i[png jpg jpeg gif]
+  validates :description, length: { minimum: 1, maximum: 500 }
 end
